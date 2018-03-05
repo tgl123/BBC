a54d967747d2d0ba56e9f50d90998e4e7438a414
tanggl <329358447@qq.com>
2018-03-05 16:25:36 +0800

    开启web功能

---
+++ src/main/java/com/holpe/bbc/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.holpe.bbc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "this is first page";
+    }
+}
