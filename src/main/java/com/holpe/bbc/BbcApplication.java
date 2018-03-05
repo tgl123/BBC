7fa28b9ea67d281e43d75832045b0e789155051c
tanggl <329358447@qq.com>
2018-03-05 12:54:52 +0800

    add project file

---
+++ src/main/java/com/holpe/bbc/BbcApplication.java
@@ -0,0 +1,12 @@
+package com.holpe.bbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbcApplication.class, args);
+	}
+}
